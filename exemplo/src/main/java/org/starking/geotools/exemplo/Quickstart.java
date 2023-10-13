6668c36780eeb32b946eb230ba9eeb7f549530bb
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-12 21:56:10 -0300

    montando o arquivo shp

--- exemplo/src/main/java/org/starking/geotools/exemplo/Quickstart.java
+++ exemplo/src/main/java/org/starking/geotools/exemplo/Quickstart.java
@@ -1,6 +1,12 @@
 package org.starking.geotools.exemplo;
 
+import java.io.File;
 import java.util.logging.Logger;
+
+import org.geotools.api.data.FileDataStore;
+import org.geotools.api.data.FileDataStoreFinder;
+import org.geotools.api.data.SimpleFeatureSource;
+import org.geotools.swing.data.JFileDataStoreChooser;
 
 /**
  * @author pedroRhamon
@@ -9,10 +15,19 @@ public class Quickstart {
 	
 	private static final Logger LOGGER= org.geotools.util.logging.Logging.getLogger(Quickstart.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		LOGGER.info("Iniciando...");
 		LOGGER.config("Bem vindo");
 		LOGGER.info("java.util.logging.config.file="+System.getProperty("java.util.logging.config.file"));
+		File file = JFileDataStoreChooser.showOpenFile("shp", null);
+		if(file == null) {
+			return;
+		}
+		
+		LOGGER.config("Arquivo selecionado: " + file);
+		
+		FileDataStore store = FileDataStoreFinder.getDataStore(file);
+		SimpleFeatureSource featureSource = store.getFeatureSource();
 	}
 
 }
