72dd9b3ec5cba92e660f39301752805076862852
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-12 21:51:45 -0300

    iniciando main()

--- exemplo/src/main/java/org/starking/geotools/exemplo/Quickstart.java
+++ exemplo/src/main/java/org/starking/geotools/exemplo/Quickstart.java
@@ -1,8 +1,18 @@
 package org.starking.geotools.exemplo;
 
+import java.util.logging.Logger;
+
 /**
  * @author pedroRhamon
  */
 public class Quickstart {
+	
+	private static final Logger LOGGER= org.geotools.util.logging.Logging.getLogger(Quickstart.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info("Iniciando...");
+		LOGGER.config("Bem vindo");
+		LOGGER.info("java.util.logging.config.file="+System.getProperty("java.util.logging.config.file"));
+	}
 
 }
