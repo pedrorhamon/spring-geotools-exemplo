b16efbfa3e81bfd44ee9e4f336161ba30b6f2b0e
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-14 23:13:43 -0300

    Ajustando coordenadas

--- exemplo/src/main/java/org/starking/geotools/exemplo/ExportKML.java
+++ exemplo/src/main/java/org/starking/geotools/exemplo/ExportKML.java
@@ -3,34 +3,45 @@ package org.starking.geotools.exemplo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.micromata.opengis.kml.v_2_2_0.Coordinate;
+import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Point;
 
 /**
  * @author pedroRhamon
  */
 public class ExportKML {
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+		// Criando um objeto KML
 		Kml kml = new Kml();
 
-		Placemark placemark = kml.createAndSetPlacemark();
-		placemark.withName("Local");
-		placemark.withDescription("Descrição do local");
+		// Criando uma lista de coordenadas
+		List<Coordinate> coordinates = new ArrayList<>();
+		coordinates.add(new Coordinate(-7.0356, -34.8339, 0.0)); // Cabedelo
+		coordinates.add(new Coordinate(-7.2383, -35.9718, 0.0)); // Campina Grande
+		coordinates.add(new Coordinate(-7.1250, -34.8733, 0.0)); // João Pessoa
 
-		// Definindo a posição (longitude e latitude)
-		Coordinate coord = new Coordinate(10.0, 20.0, 0.0);
-		placemark.createAndSetPoint().addToCoordinates(coord.toString());
+		// Criando um documento KML
+		Document document = kml.createAndSetDocument();
+
+		// Adicionando os pontos ao documento KML
+		for (int i = 0; i < coordinates.size(); i++) {
+			Coordinate coordinate = coordinates.get(i);
+			Placemark placemark = document.createAndAddPlacemark();
+			placemark.withName("Ponto " + (i + 1));
+			Point point = placemark.createAndSetPoint();
+			point.addToCoordinates(coordinate.getLongitude(), coordinate.getLatitude());
+		}
 
 		// Salvando o KML
-		try {
-			kml.marshal(new File("C:\\Users\\pedro\\Downloads\\arquivo.kml"));
-			System.out.println("Arquivo KML exportado com sucesso.");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		kml.marshal(new File("C:\\Users\\pedro\\Downloads\\arquivo.kml"));
+
+		System.out.println("Arquivo KML exportado com sucesso.");
 	}
 }
