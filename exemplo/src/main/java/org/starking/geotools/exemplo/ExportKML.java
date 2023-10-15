05d920cb5766666e5dce5472307927a6e9ffb2fa
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-14 22:27:22 -0300

    montando o Placemark

--- exemplo/src/main/java/org/starking/geotools/exemplo/ExportKML.java
+++ exemplo/src/main/java/org/starking/geotools/exemplo/ExportKML.java
@@ -5,22 +5,32 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.micromata.opengis.kml.v_2_2_0.*;
+
 /**
  * @author pedroRhamon
  */
 public class ExportKML {
-	
+
 	public static void main(String[] args) {
+
+		Kml kml = new Kml();
+
+		Placemark placemark = kml.createAndSetPlacemark();
+		placemark.withName("Local");
+		placemark.withDescription("Descrição do local");
 		
+		// Definindo a posição (longitude e latitude)
+        Coordinate coord = new Coordinate(10.0, 20.0, 0.0);
+        placemark.createAndSetPoint().addToCoordinates(coord);
+
 		try {
-			
-			File kmlFile = new File("C:\\Users\\pedro\\Downloads\\arquivo.kml");
-			Map<String, Serializable> params = new HashMap<>();
-			
+
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 	}
 
 }
