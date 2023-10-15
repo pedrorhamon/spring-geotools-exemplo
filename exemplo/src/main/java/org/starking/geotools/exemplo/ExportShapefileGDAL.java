390edbe82ba459036502da899d9e2789a56a19cf
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-15 19:52:41 -0300

    abrindo shapefile

--- exemplo/src/main/java/org/starking/geotools/exemplo/ExportShapefileGDAL.java
+++ exemplo/src/main/java/org/starking/geotools/exemplo/ExportShapefileGDAL.java
@@ -7,13 +7,18 @@ import org.gdal.ogr.ogr;
  * @author pedroRhamon
  */
 public class ExportShapefileGDAL {
-	
+
 	public static void main(String[] args) {
-		 ogr.RegisterAll();
-		 
-		 // Abre o shapefile
-	        String shapefilePath = "C:\\Users\\pedro\\Downloads\\shapefileGDAL\\shapefile.shp";
-	        DataSource ds = ogr.Open(shapefilePath);
+		ogr.RegisterAll();
+
+		// Abre o shapefile
+		String shapefilePath = "C:\\Users\\pedro\\Downloads\\shapefileGDAL\\shapefile.shp";
+		DataSource ds = ogr.Open(shapefilePath);
+
+		if (ds == null) {
+			System.err.println("Erro ao abrir o shapefile.");
+			System.exit(1);
+		}
 	}
 
 }
