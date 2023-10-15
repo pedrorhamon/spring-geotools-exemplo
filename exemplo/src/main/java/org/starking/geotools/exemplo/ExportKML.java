9cf6333c69e45791f82558153870d829c03d3e15
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-10-14 22:14:26 -0300

    Iniciando map params

--- exemplo/src/main/java/org/starking/geotools/exemplo/ExportKML.java
+++ exemplo/src/main/java/org/starking/geotools/exemplo/ExportKML.java
@@ -1,11 +1,25 @@
 package org.starking.geotools.exemplo;
 
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author pedroRhamon
  */
 public class ExportKML {
 	
 	public static void main(String[] args) {
+		
+		try {
+			
+			File kmlFile = new File("C:\\Users\\pedro\\Downloads\\arquivo.kml");
+			Map<String, Serializable> params = new HashMap<>();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 	}
 
